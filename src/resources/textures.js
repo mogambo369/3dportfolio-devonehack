c9762013afa77f872f5668f2b0d17a02b1a72245
Dakshata <22-783@apsdhaulakuan.onmicrosoft.com>
2025-09-27 02:23:27 +0530

    feat: Implement keyboard and touchscreen controls for movement

---
+++ src/resources/textures.js
@@ -0,0 +1,58 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = "../src/jsm/terpSolutions.png";
+billboardTextures.bagHolderBetsTexture =
+  "../src/jsm/Bagholdersbetsbillboard.png";
+billboardTextures.homeSweetHomeTexture =
+  "../src/jsm/home-sweet-home-portrait.png";
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = "../src/jsm/githubLogo.png";
+boxTexture.twitter = "../src/jsm/twitter.png";
+boxTexture.LinkedIn = "../src/jsm/linkedInLogo.png";
+boxTexture.mail = "../src/jsm/envelope.png";
+boxTexture.globe = "../src/jsm/thunder.png";
+boxTexture.reactIcon = "../src/jsm/react.png";
+boxTexture.allSkills = "../src/jsm/allSkills.png";
+boxTexture.lensFlareMain = "../src/jsm/lensflare0.png";
+boxTexture.skrillex = "../src/jsm/skrillex.png";
+boxTexture.edmText = "../src/jsm/EDM.png";
+boxTexture.writing = "../src/jsm/writing.png";
+
+//material textures
+let stoneTexture = "../src/jsm/stone.png";
+let woodTexture = "../src/jsm/woodTexture.jpg";
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = "../src/jsm/terp-solutions-text.png";
+inputText.activities = "../src/jsm/activities_text.png";
+inputText.bagholderBetsText = "../src/jsm/bagholderbets-text.png";
+inputText.homeSweetHomeText = "../src/jsm/home-sweet-home-text.png";
+inputText.staticPortfolio = "../src/jsm/static-portfolio.png";
+
+//SVG
+let SVG = {};
+SVG.reactLogo = "../src/jsm/react-svg.svg";
+
+//URLs
+let URL = {};
+URL.terpsolutions = "https://github.com/mogambo369";
+URL.bagholderBets = "https://github.com/mogambo369";
+URL.homeSweetHomeURL = "https://github.com/mogambo369";
+URL.gitHub = "https://github.com/mogambo369";
+URL.twitter = "https://x.com/idakshata_";
+URL.email = "https://mailto:dakshata@gmail.com";
+URL.githubBagholder = "https://github.com/mogambo369";
+URL.githubHomeSweetHome = "https://github.com/mogambo369";
+URL.devTo = "https://github.com/mogambo369";
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
